b4c012255f94ed6d419c2302ad85adb2ffb5ce5e
Shailesh <105206096+ShaileshS4@users.noreply.github.com>
2024-03-10 18:51:08 +0530

    Add files via upload

---
+++ firstprogram.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Shailesh S Sangolkar
+#
+# Created:     27-07-2022
+# Copyright:   (c) Shailesh S Sangolkar 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+# print("hello coders")
+# print('i know how to count 1,2,3,...... ')
+
+name = "Shailesh"
+age = 23
+print (name)
+print (age)
